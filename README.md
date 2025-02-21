d70cbc87b970ac0fb5d9dc9f3c587fb95bd47bd8
anasrazaofficial <anasbukhari4114@gmail.com>
2025-02-21 16:26:44 +0500

    Push code to repo

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # python
+
+## Basic
+
+- **print(val1)** is used to print
+- **input(message)** is used to take input
---
+++ basic.py
@@ -0,0 +1,5 @@
+name = input("What is the patient name?: ")
+age = input("And his/her age: ")
+isNew = input("Is he a new patient(Y/N): ")
+
+print(name + " is " + age + " years old.")
